a04abb8fbfd708dc639a81cf0e95ac4f22724abb
Sanghack Lee <sanghack.lee@gmail.com>
2024-04-06 00:20:35 +0900

    parallelize more

--- scripts/conduct_experiments.py
+++ scripts/conduct_experiments.py
@@ -1,14 +1,10 @@
 import itertools
 import multiprocessing as mp
 from pathlib import Path
+
 from joblib import Parallel, delayed
 
-from cddd.experiments.FN_experiment import fn_experiment
-from cddd.experiments.PC_experiment import pc_experiment
 from cddd.experiments.PC_stable_experiment import pc_stable_experiment
-from cddd.experiments.Complete_PC_stable_experiment import complete_pc_stable_experiment
-from cddd.experiments.cond_experiment import cond_experiment
-from cddd.experiments.new_FN_experiment import new_fn_experiment
 
 if __name__ == '__main__':
     ############# just for checking working! #####################
@@ -16,12 +12,13 @@ if __name__ == '__main__':
 
     WORKING_DIR = '/home/jonghwan/CDDD'
     # WORKING_DIR = '/Users/gimjonghwan/Desktop/CD_DD'
-    # '/Users/sanghacklee/Dropbox/python_projs/CD_DD'
+    # WORKING_DIR = '/Users/sanghacklee/Dropbox/python_projs/CD_DD'
     results_dir = f'{WORKING_DIR}/results'
     Path(results_dir).mkdir(parents=True, exist_ok=True)
 
     if EXPERIMENTAL_RUN:
         BNs = ['Linear_20_36', 'Linear_20_64', 'Linear_20_36_sf', 'Linear_20_64_sf']
+        # BNs = ['Linear_20_36']
         CITs = ['ParCorr'] * len(BNs)
 
         # BNs = ['alarm', 'sachs', 'insurance']
@@ -29,6 +26,8 @@ if __name__ == '__main__':
         # nums_vars = (20,)
         # edge_ratios = (1.2, 1.5, 2)
         # dataset_sizes = (200, 500, 1000)
+        Ks = [0, 1, 2]
+        Alphas = [0.05, 0.01]
         dataset_sizes = (30, 50, 100, 150)
         num_sampling = 10
 
@@ -50,11 +49,11 @@ if __name__ == '__main__':
     # Parallel(n_jobs=mp.cpu_count())(delayed(new_fn_experiment)(BN, WORKING_DIR, dataset_sizes, num_sampling) for BN in BNs)
     Parallel(n_jobs=n_jobs)(
         itertools.chain(
-            #(delayed(cond_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
-            #(delayed(pc_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
-            (delayed(pc_stable_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
-            #(delayed(complete_pc_stable_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
-            #(delayed(new_fn_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs))
+            # (delayed(cond_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
+            # (delayed(pc_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
+            (delayed(pc_stable_experiment)(BN, cit, WORKING_DIR, size_of_sampled_dataset, num_sampling, K, Alpha) for (BN, cit), K, Alpha, size_of_sampled_dataset in itertools.product(list(zip(BNs, CITs)), Ks, Alphas, dataset_sizes)),
+            # (delayed(complete_pc_stable_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs)),
+            # (delayed(new_fn_experiment)(BN, cit, WORKING_DIR, dataset_sizes, num_sampling) for BN, cit in zip(BNs, CITs))
         )
     )
-    #fn_experiment(WORKING_DIR, nums_vars, edge_ratios, dataset_sizes)
+    # fn_experiment(WORKING_DIR, nums_vars, edge_ratios, dataset_sizes)
