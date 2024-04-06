0235f3c612783d48d1d63bcfccfca16100127c99
sanghacklee <sanghack.lee@gmail.com>
2024-04-06 21:58:27 +0900

    anonymize

--- src/cddd/utils.py
+++ src/cddd/utils.py
@@ -7,7 +7,7 @@ from filelock import FileLock
 
 def safe_save_to_csv(result: List[List], columns: List[str], file_path: str):
     df = pd.DataFrame(result, columns=columns)
-    lock = FileLock(file_path)
+    lock = FileLock(file_path + '.lock')
     with lock:
         if not os.path.exists(file_path):
             df.to_csv(file_path, mode='w')
