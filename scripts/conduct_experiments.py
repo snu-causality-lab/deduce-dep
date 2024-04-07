c0405e3499f0f57ec84ba3d9e5341d526ea02e19
Sanghack Lee <sanghack.lee@gmail.com>
2024-04-07 13:16:06 +0900

    change conduct experiments only for full experiments

--- scripts/conduct_experiments.py
+++ scripts/conduct_experiments.py
@@ -11,42 +11,24 @@ from cddd.experiments.cond_experiment import cond_experiment
 from cddd.experiments.new_FN_experiment import new_fn_experiment
 
 if __name__ == '__main__':
-    ############# just for checking working! #####################
-    EXPERIMENTAL_RUN = True
-
     n_jobs = mp.cpu_count()
     WORKING_DIR = '/home/johndoe/CD_DD'
     results_dir = f'{WORKING_DIR}/results'
     Path(results_dir).mkdir(parents=True, exist_ok=True)
 
-    if EXPERIMENTAL_RUN:
-        BNs = ['Linear_20_36', 'Linear_20_36_sf']
-        # we must specify type of CITs,
-        # `ParCorr` for partial correlation,
-        # `G2` for discrete data,
-        # `KCI` for kernel conditional independence test (using RBF kernel with median heuristic).
-        CITs = ['ParCorr'] * len(BNs)
-
-        Ks = [0, ]
-        Alphas = [0.05, 0.01]
-        dataset_sizes = (100,)
-        num_sampling = 5
-        reliability_criteria = ['no', 'deductive_reasoning']
-
-    else:
-        # full (+additional) experimentation (this may not include tests reported during the rebuttal)
-        BNs = ['ER_10_12', 'ER_10_15', 'ER_10_20',
-               'ER_20_24', 'ER_20_30', 'ER_20_40',
-               'ER_30_36', 'ER_30_45', 'ER_30_60',
-               'alarm', 'insurance', 'sachs', 'asia', 'child', 'water']
-        CITs = ['G2'] * len(BNs)
-        nums_vars = (10, 20, 30)
-        Ks = [0, 1, 2]
-        Alphas = [0.05, 0.01]
-        edge_ratios = (1.2, 1.5, 2)
-        dataset_sizes = (200, 500, 1000, 2000)
-        num_sampling = 30
-        reliability_criteria = ['no', 'deductive_reasoning']
+    # full (+additional) experimentation (this may not include tests reported during the rebuttal)
+    BNs = ['ER_10_12', 'ER_10_15', 'ER_10_20',
+           'ER_20_24', 'ER_20_30', 'ER_20_40',
+           'ER_30_36', 'ER_30_45', 'ER_30_60',
+           'alarm', 'insurance', 'sachs', 'asia', 'child', 'water']
+    CITs = ['G2'] * len(BNs)
+    nums_vars = (10, 20, 30)
+    Ks = [0, 1, 2]
+    Alphas = [0.05, 0.01]
+    edge_ratios = (1.2, 1.5, 2)
+    dataset_sizes = (200, 500, 1000, 2000)
+    num_sampling = 30
+    reliability_criteria = ['no', 'deductive_reasoning']
 
     # Sorry for some inconsistencies in the order of arguments &
     # also some of the `options' are already specified inside the functions,
