8be2075699b25f09d834a0e809d5f80d6b0efa85
JongHwan-K <rlawhdghkk@naver.com>
2024-06-03 11:34:49 +0900

    remove unnecessary class and functions in sampling.py

--- src/cddd/sampling.py
+++ src/cddd/sampling.py
@@ -20,41 +20,6 @@ def shuffled(xs: Iterable[T]) -> List[T]:
     random.shuffle(xs)
     return xs
 
-
-def unions(xss: Collection[AbstractSet[T]]) -> Set[T]:
-    out = set()
-    for xs in xss:
-        out |= xs
-    return out
-
-
-def logical_and(xs):
-    iter_xs = iter(xs)
-    out = next(iter_xs)
-    for x in iter_xs:
-        out &= x
-    return out
-
-
-class visited_queue(Generic[H]):
-    def __init__(self, xs: Collection[H]):
-        self.queue = deque(xs)
-        self.visited = set(xs)
-
-    def extend(self, xs: Collection[H]):
-        xs = set(xs) - self.visited
-        self.visited |= xs
-        for x in xs:
-            self.queue.append(x)
-
-    def pop(self) -> H:
-        return self.queue.pop()
-
-    def __bool__(self):
-        return bool(self.queue)
-
-
-# assume
 class BayesianNetwork:
     def __init__(self, G: nx.DiGraph, CPTs: Dict[int, CPTType]):
         self.G = G
@@ -77,76 +42,6 @@ class BayesianNetwork:
                 instance[v_i] = xxx[0]  # use self.prob to sample a value of the variable.
 
         return pd.DataFrame(instances, columns=list(range(self.n_variables)))
-
-    def is_d_separated(self,
-                       Xs: AbstractSet[int],
-                       Ys: AbstractSet[int],
-                       Zs: AbstractSet[int] = frozenset()) -> bool:
-        # test xs _||_ ys | zs
-        Xs, Ys = Xs - Zs, Ys - Zs
-        if Xs & Ys:
-            return False
-        return all(self.__is_d_separated(x, y, Zs) for x, y in product(Xs, Ys))
-
-    @lru_cache
-    def Pa(self, u):
-        return frozenset(self.G.predecessors(u))
-
-    @lru_cache
-    def Ch(self, u):
-        return frozenset(self.G.successors(u))
-
-    @lru_cache
-    def An(self, x):
-        return frozenset({x} |
-                         self.Pa(x) |
-                         unions([self.An(z) for z in self.Pa(x)]))
-
-    @lru_cache
-    def De(self, x):
-        return frozenset({x} |
-                         self.Ch(x) |
-                         unions([self.De(z) for z in self.Ch(x)]))
-
-    def __is_d_separated(self,
-                         X: int,
-                         Y: int,
-                         Zs: AbstractSet[int] = frozenset()) -> bool:
-        # test x _||_ y | zs
-        assert X not in Zs
-        assert Y not in Zs
-        assert X != Y
-
-        queue = visited_queue([('>', X), ('<', X)])
-        while queue:
-            arrow_head, at = queue.pop()
-            if at == Y:
-                return False
-            if arrow_head == '>':
-                if at not in Zs:  # -> ->
-                    queue.extend([('>', _) for _ in self.Ch(at)])
-                if self.De(at) & Zs:  # -> <- only if the collider is active
-                    queue.extend([('<', _) for _ in self.Pa(at)])
-
-            elif at not in Zs:  # <- ->, or <- <-
-                queue.extend([('>', _) for _ in self.Ch(at)])
-                queue.extend([('<', _) for _ in self.Pa(at)])
-
-        return True
-
-
-# simply, perform chi-square test for each condition
-def check_conditional_independence(data: pd.DataFrame, X, Y, Zs: AbstractSet[int] = frozenset(), alpha=0.05) -> bool:
-    # xs, ys, zs be sets of variables (column indices) to check xs _||_ ys | zs
-    for zs in product([0, 1], repeat=len(Zs)):
-        condition = logical_and([data[Z] == z for Z, z in zip(Zs, zs)])
-        selected = data[condition] if Zs else data
-        _, p, *_ = chi2_contingency(pd.crosstab(selected[X], selected[Y]))
-        if p < alpha:
-            return False
-
-    return True
-
 
 def random_DAG(num_vars: int, num_edges: int) -> nx.DiGraph:
     # randomly generate an acyclic graph
